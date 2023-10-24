c5762802562a8c08a98ab6cabbeef01f27be4ed7
Jeff Zhao <jeff.no.zhao@gmail.com>
2023-10-23 20:51:32 -0400

    add version

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,9 @@
+fn print_version() -> Result<i32, AppError> {
+    let version = env!("CARGO_PKG_VERSION");
+    writeln!(&mut std::io::stdout(), "{PROGRAM_NAME}-{version}")?;
+    Ok(0)
+}
+
 fn main() {
     println!("Hello, world!");
 }
